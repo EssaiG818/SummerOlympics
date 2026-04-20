12db9fa9836b4854b19a9948e9897dfc367f7b8a
EssaiG818 <egonzalez7818@gmail.com>
2026-04-20 11:03:34 -0700

    Initial Commit

---
+++ SummerOlympics/SummerOlympicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummerOlympicsApp.swift
+//  SummerOlympics
+//
+//  Created by GONZALEZ, ESSAI on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct SummerOlympicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SummerOlympicsTests/SummerOlympicsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SummerOlympicsTests.swift
+//  SummerOlympicsTests
+//
+//  Created by GONZALEZ, ESSAI on 4/20/26.
+//
+
+import Testing
+@testable import SummerOlympics
+
+struct SummerOlympicsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
